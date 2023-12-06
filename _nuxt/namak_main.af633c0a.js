9974ab55979c66d3dc280b63e7868965e528efc9
Merry Shirvani <mehrshi@cs.ubc.ca>
2023-12-05 23:43:26 -0800

    Fix build

--- _nuxt/namak_main.af633c0a.js
+++
@@ -1,1 +0,0 @@
-import"./entry.58c458cc.js";const _=""+globalThis.__publicAssetsURL("senior_main.png"),i=""+globalThis.__publicAssetsURL("mentoring_main.png"),n=""+globalThis.__publicAssetsURL("tmi_main.png"),o=""+globalThis.__publicAssetsURL("namak_main.png");export{_,i as a,n as b,o as c};
